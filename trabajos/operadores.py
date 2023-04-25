9c4a4264375d36678d2a846dbf7ad565b8593a66
KDOE-117 <jdn2037@gmail.com>
2023-04-25 08:31:08 -0500

    trabajo

---
+++ trabajos/operadores.py
@@ -0,0 +1,42 @@
+#punto1
+#ejercicio1
+ejercicio1= 40 + 2 * 7 ^ 2
+print(ejercicio1)
+#ejercicio2
+ejercicio2= 3+ (2+5)*1+ (3-2*3)
+print(ejercicio2)
+#ejecicicio3
+ejercicio3= 16 - 8 * (10 - (5 + 4))
+print(ejercicio3)
+#ejercicio4
+ejercicio4=(5+3*2)-(7*2)*(3+ (2*3-1))
+print(ejercicio4)
+#ejercicio5
+ejercicio5=(24+220)/3*2
+print(ejercicio5)
+#ejercicio6
+ejercicio6=(6/3*2)-(7*2)+ (2*(3+ (2/3-1)*4))
+print(ejercicio6)
+
+#punto2
+#punto a
+ejercicioa= 29.7 + 5.0 ** 2.0
+print(ejercicioa)
+#punto b
+ejerciciob= ((2 - 3) * 4 * 5 / (4 + 3 * 9) )
+print(ejerciciob)
+#punto c
+ejercicioc= 49.38 + 127.73 - 15.02 * 6.83 / 3.22
+print(ejercicioc)
+#punto d
+ejerciciod= 19 / 3
+print(ejerciciod)
+#punto e
+ejercicioe= 15 % 6
+print(ejercicioe)
+#punto f
+ejerciciof=((15 * 8 % 6 + 24 / 2 * 3) * 3 / 4) * (5 * 1 / 2 + 1 / 4 + 2 * 3 - 4)
+print(ejerciciof)
+
+
+
